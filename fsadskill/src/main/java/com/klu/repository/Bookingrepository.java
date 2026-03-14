a230f0cb64ca4684a90e8375331eaf8a0a09cdb3
Pranathi798154 <2400032306@kluniversity.in>
2026-03-14 19:17:23 +0530

    detach
    detach

--- fsadskill/src/main/java/com/klu/repository/Bookingrepository.java
+++
@@ -1,9 +0,0 @@
-package com.klu.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.klu.model.Bookingmodel;
-
-public interface Bookingrepository extends JpaRepository<Bookingmodel, Integer>
-{
-
-}
